219b01f98a1e992f175c9d9c967d7c08077c4e8e
pavankalyan7288 <139549312+pavankalyan7288@users.noreply.github.com>
2023-07-17 15:36:07 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,4 @@
  <?php
 
-$dbhost= "localhost";
-$dbuser= "root";
-$dbpass = "";
-$dbname = "login_sample_db";
-
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-if (!$conn) {
-
-    echo "Connection failed!";
-
+this is pavan kalyan ;
 }
